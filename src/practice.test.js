dafddbd94e00e5bdb737931f932da7abe2754852
Jason <jasonbetsargon@gmail.com>
2025-05-21 19:07:20 -0700

    Complete all jest tests

--- src/practice.test.js
+++ src/practice.test.js
@@ -1,4 +1,5 @@
-import { sum, capitalize } from "./testPractice.js";
+import { experiments } from "webpack";
+import { sum, capitalize, reverseString, Calculator, caesarCipher, analyzeArray } from "./testPractice.js";
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
@@ -13,16 +14,27 @@ test('reverses string', () => {
 
 describe('calculator operations', () => {
   test('add', () => {
+    const calculator = new Calculator;
     expect(calculator.add(5, 6)).toBe(11);
   });
   test('subtract', () => {
+    const calculator = new Calculator;
     expect(calculator.subtract(4, 2)).toBe(2);
   });
   test('divide', () => {
+    const calculator = new Calculator;
     expect(calculator.divide(6, 2)).toBe(3);
   })
   test('multiply', () => {
+    const calculator = new Calculator;
     expect(calculator.multiply(3, 2)).toBe(6);
   })
 });
 
+test('caesar cipher', () => {
+  expect(caesarCipher('xyz!', 3)).toBe('abc!');
+});
+
+test('analyze array', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({average: 4, min: 1, max: 8, length: 6});
+});
--- src/testPractice.js
+++ src/testPractice.js
@@ -7,3 +7,64 @@ export function capitalize(str) {
   if(!str) return null;
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+export class Calculator {
+  add(num1, num2) {
+    return num1 + num2;
+  }
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+  multiply(num1, num2) {
+    return num1 * num2;
+  }
+  divide(num1, num2) {
+    return num1 / num2;
+  }
+}
+
+export function caesarCipher(text, shift) {
+  let result = '';
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+    if (char.match(/[a-z]/i)) {
+      const code = text.charCodeAt(i);
+      let shiftedCode;
+
+      if (code >= 65 && code <= 90) { // Uppercase letters
+        shiftedCode = ((code - 65 + shift) % 26) + 65;
+      } else if (code >= 97 && code <= 122) { // Lowercase letters
+        shiftedCode = ((code - 97 + shift) % 26) + 97;
+      }
+      result += String.fromCharCode(shiftedCode);
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
+
+export function analyzeArray(arr) {
+  let arrObject = {
+    average: null,
+    min: null,
+    max: null,
+    length: null
+  };
+  if(!arr) return arrObject;
+  let sum = 0;
+  arrObject.max, arrObject.min = arr[0];
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if(arr[i] > arrObject.max) arrObject.max = arr[i];
+    if(arr[i] < arrObject.min) arrObject.min = arr[i];
+    arrObject.length++;
+  }
+  arrObject.average = sum / arrObject.length;
+  return arrObject;
+}
